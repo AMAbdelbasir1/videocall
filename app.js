4a583237621237735308d57b8c5d4f53c78ee416
AMAbdelbasir1 <addtooo140@gmail.com>
2023-12-04 21:06:17 +0200

    try new way

--- app.js
+++ app.js
@@ -10,20 +10,58 @@ const io = require("socket.io")(server);
 
 io.on("connection", (socket) => {
   console.log("server connected");
-  socket.on("join", (vedioId) => {
-    socket.join(vedioId);
+  socket.on("join", (videoId) => {
+    socket.join(videoId);
   });
-  socket.on("videorequest", (vedioId) => {
-    socket.broadcast.to(vedioId).emit("getPeerId");
+  socket.on("videorequest", (videoId) => {
+    socket.broadcast.to(videoId).emit("getPeerId");
   });
   socket.on("sendPeerId", (data) => {
-    socket.broadcast.to(data.videoid).emit("recivePeerId", data.peerid);
+    console.log("recived", data);
+    socket.broadcast.to(data.videoId).emit("recivePeerId", data.peerId);
+  });
+  socket.on("newUserJoin", () => {
+    io.emit("newUserJoin");
   });
 });
 
 app.get("/video", (req, res) => {
-  res.render("video", { videoid: "video-1122" });
+  res.render("video", { videoId: "video-1122" });
 });
+
 server.listen(process.env.PORT || 3000, () => {
   console.log(`videocall app listening at ${3000}`);
 });
+
+// const express = require("express");
+// const app = express();
+// const server = require("http").createServer(app);
+
+// app.set("view engine", "ejs");
+// app.use(express.static("public"));
+// app.use(express.urlencoded({ extended: true }));
+
+// const io = require("socket.io")(server);
+
+// io.on("connection", (socket) => {
+//   console.log("server connected");
+//   socket.on("join", (vedioId) => {
+//     socket.join(vedioId);
+//   });
+//   socket.on("videorequest", (vedioId) => {
+//     socket.broadcast.to(vedioId).emit("getPeerId");
+//   });
+//   socket.on("sendPeerId", (data) => {
+//     socket.broadcast.to(data.videoid).emit("recivePeerId", data.peerid);
+//   });
+//   socket.on("newUserJoin", () => {
+//     io.emit("newUserJoin");
+//   });
+// });
+
+// app.get("/video", (req, res) => {
+//   res.render("video", { videoid: "video-1122" });
+// });
+// server.listen(process.env.PORT || 3000, () => {
+//   console.log(`videocall app listening at ${3000}`);
+// });
--- public/init.js
+++ public/init.js
@@ -1,33 +1,24 @@
-const callbtn = document.getElementById("callbtn");
 const toggleAudioBtn = document.getElementById("toggleAudioBtn");
 const toggleVideoBtn = document.getElementById("toggleVideoBtn");
 const videoContainer = document.getElementById("video-container");
-const vedioId = document.getElementById("vedioId").value;
+const videoId = document.getElementById("videoId").value;
 const socket = io();
 let peer = new Peer();
 let peerId = null;
+let localStream; // Store the local stream to toggle audio and video
+
+// Array to store the video elements created for each call
+const videoElements = [];
 
 socket.on("connect", () => {
   console.log("client connected");
-  socket.emit("join", vedioId);
+  socket.emit("join", videoId);
 });
 
 peer.on("open", (id) => {
   console.log("My peer ID is: " + id);
   peerId = id;
 });
-
-callbtn.onclick = () => {
-  socket.emit("videorequest", vedioId);
-};
-
-socket.on("getPeerId", () => {
-  socket.emit("sendPeerId", {
-    videoid: vedioId,
-    peerid: peerId,
-  });
-});
-let localStream; // Store the local stream to toggle audio and video
 
 toggleAudioBtn.onclick = () => {
   localStream.getAudioTracks().forEach((track) => {
@@ -40,8 +31,20 @@ toggleVideoBtn.onclick = () => {
     track.enabled = !track.enabled;
   });
 };
+
+// Handle the button click to share your video
+document.getElementById("shareVideoBtn").onclick = () => {
+  // Emit the peer ID to the server for new user handling
+  console.log("clicked");
+  socket.emit("sendPeerId", {
+    videoId: videoId,
+    peerId: peerId,
+  });
+};
+
 socket.on("recivePeerId", (id) => {
   console.log("peer id user 2 " + id);
+
   // Check if the user has a camera
   const mediaConstraints = {
     audio: true,
@@ -99,25 +102,151 @@ peer.on("call", (call) => {
     });
 });
 
-const videoElements = [];
-
 function showVideo(stream) {
-  // Check if a video element already exists for this stream
-  let existingVideo = videoElements.find(
-    (element) => element.srcObject === stream,
-  );
-
-  if (!existingVideo) {
-    // If not, create a new video element
-    let video = document.createElement("video");
-    video.srcObject = stream;
-    video.autoplay = true;
-    video.playsinline = true;
-    video.muted = false; // Set to true if you want to mute the local user's video
-    videoElements.push(video);
-
-    // Append the video element to the video container
-    videoContainer.appendChild(video);
-  }
-  callbtn.remove();
+  let video = document.createElement("video");
+  video.srcObject = stream;
+  videoContainer.appendChild(video);
+  video.play();
 }
+
+// Function to handle new user join
+function handleNewUserJoin() {
+  // Clear the video container when a new user joins
+  videoContainer.innerHTML = "";
+
+  // Loop through existing video elements and recreate them
+  videoElements.forEach((element) => {
+    const callerId = element.id.replace("video-", "");
+    showVideo(element.srcObject, callerId);
+  });
+}
+
+// Socket.IO event for handling new user join
+socket.on("newUserJoin", () => {
+  handleNewUserJoin();
+});
+
+// const callbtn = document.getElementById("callbtn");
+// const toggleAudioBtn = document.getElementById("toggleAudioBtn");
+// const toggleVideoBtn = document.getElementById("toggleVideoBtn");
+// const videoContainer = document.getElementById("video-container");
+// const vedioId = document.getElementById("vedioId").value;
+// const socket = io();
+// let peer = new Peer();
+// let peerId = null;
+
+// socket.on("connect", () => {
+//   console.log("client connected");
+//   socket.emit("join", vedioId);
+// });
+
+// peer.on("open", (id) => {
+//   console.log("My peer ID is: " + id);
+//   peerId = id;
+// });
+
+// callbtn.onclick = () => {
+//   socket.emit("videorequest", vedioId);
+// };
+
+// socket.on("getPeerId", () => {
+//   socket.emit("sendPeerId", {
+//     videoid: vedioId,
+//     peerid: peerId,
+//   });
+// });
+// let localStream; // Store the local stream to toggle audio and video
+
+// toggleAudioBtn.onclick = () => {
+//   localStream.getAudioTracks().forEach((track) => {
+//     track.enabled = !track.enabled;
+//   });
+// };
+
+// toggleVideoBtn.onclick = () => {
+//   localStream.getVideoTracks().forEach((track) => {
+//     track.enabled = !track.enabled;
+//   });
+// };
+// socket.on("recivePeerId", (id) => {
+//   console.log("peer id user 2 " + id);
+//   // Check if the user has a camera
+//   const mediaConstraints = {
+//     audio: true,
+//     video: { facingMode: "user" }, // Specify the user-facing camera
+//   };
+
+//   navigator.mediaDevices
+//     .getUserMedia(mediaConstraints)
+//     .then((stream) => {
+//       localStream = stream;
+//       let call = peer.call(id, stream);
+//       call.on("stream", showVideo);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       // If there's an error, try getting audio-only stream
+//       return navigator.mediaDevices.getUserMedia({ audio: true });
+//     })
+//     .then((audioStream) => {
+//       localStream = audioStream;
+//       let call = peer.call(id, audioStream);
+//       call.on("stream", showVideo);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+//     socket.emit("newUserJoin");
+// });
+
+// peer.on("call", (call) => {
+//   // Answer the call, providing our mediaStream
+//   const mediaConstraints = {
+//     audio: true,
+//     video: { facingMode: "user" }, // Specify the user-facing camera
+//   };
+
+//   navigator.mediaDevices
+//     .getUserMedia(mediaConstraints)
+//     .then((stream) => {
+//       localStream = stream;
+//       call.answer(stream);
+//       call.on("stream", showVideo);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       // If there's an error, answer with audio-only stream
+//       return navigator.mediaDevices.getUserMedia({ audio: true });
+//     })
+//     .then((audioStream) => {
+//       localStream = audioStream;
+//       call.answer(audioStream);
+//       call.on("stream", showVideo);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
+
+// const videoElements = [];
+
+// function showVideo(stream) {
+//   // Check if a video element already exists for this stream
+//   let existingVideo = videoElements.find(
+//     (element) => element.srcObject === stream,
+//   );
+
+//   if (!existingVideo) {
+//     // If not, create a new video element
+//     let video = document.createElement("video");
+//     video.srcObject = stream;
+//     video.autoplay = true;
+//     video.playsinline = true;
+//     video.muted = false; // Set to true if you want to mute the local user's video
+//     videoElements.push(video);
+
+//     // Append the video element to the video container
+//     videoContainer.appendChild(video);
+//   }
+//   callbtn.remove();
+// }
